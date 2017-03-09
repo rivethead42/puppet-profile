7875a0a5d95adca5cafe682ee6e6162a561f6534
Travis N. Thomse n <travis@linuxacademy.com>
2017-03-09 03:34:25 +0000

    update base add selinux and apache

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,6 +1,15 @@
-class profile::base {
-  include ssh
+class profile::base(
+  $ntp_servers = [
+    '0.ubuntu.pool.ntp.org',
+    '1.ubuntu.pool.ntp.org',
+  ],
+) {
+  include ::ssh
   class { '::ntp':
-    servers => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org' ],
+    servers => $ntp_servers,
+  }
+  
+  if $facts['os']['family'] == 'RedHat' {
+    include ::profile::selinux
   }
 }
