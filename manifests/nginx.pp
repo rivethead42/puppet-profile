3b3ef20c20ebe2bb5be27eaf3789e7a607772e97
Travis N. Thomse n <travis@linuxacademy.com>
2017-03-13 20:34:22 +0000

    bla

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -3,9 +3,10 @@ class profile::nginx(
 ) {
   include ::nginx
   $vhosts.each |$vhost| {
-    ::nginx::vhost { $vhost['fqdn']:
-      port            => $vhost['port'],
-      server_aliases  => $vhost['server_aliases']
-    }
+    notice($vhost)
+    #::nginx::vhost { $vhost['fqdn']:
+     # port            => $vhost['port'],
+     # server_aliases  => $vhost['server_aliases']
+    #}
   }
 }
