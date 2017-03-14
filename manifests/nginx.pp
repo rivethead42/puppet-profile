c26e52cd946e38b2220a3558213a5dd4bbd48daf
Travis N. Thomse n <travis@linuxacademy.com>
2017-03-14 01:04:10 +0000

    change to an array

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,15 +1,14 @@
 class profile::nginx(
-  Hash $vhosts,
+  Array $vhosts,
 ) {
   include ::nginx
   notify { "testdata":
     message => $vhosts;
   }
-  #$vhosts.each |$vhost| {
-  #  notice($vhost)
-    #::nginx::vhost { $vhost['fqdn']:
-     # port            => $vhost['port'],
-     # server_aliases  => $vhost['server_aliases']
-    #}
-  #}
+  $vhosts.each |$vhost| {
+    ::nginx::vhost { $vhost['fqdn']:
+      port            => $vhost['port'],
+      server_aliases  => $vhost['server_aliases']
+    }
+  }
 }
