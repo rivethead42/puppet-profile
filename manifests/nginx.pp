e183ef766a5025f030fcd491d04f9182afac429e
Travis N. Thomse n <travis@linuxacademy.com>
2017-03-14 00:47:45 +0000

    add debug info

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -2,6 +2,9 @@ class profile::nginx(
   Hash $vhosts,
 ) {
   include ::nginx
+  notify { "testdata":
+    messag => $hosts;
+  }
   $vhosts.each |$vhost| {
     notice($vhost)
     #::nginx::vhost { $vhost['fqdn']:
