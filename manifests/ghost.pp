a26b7827082c9d468ef75fe1ee2e00973c6e3e73
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-04-04 12:28:03 -0500

    add user and group

--- manifests/ghost.pp
+++ manifests/ghost.pp
@@ -3,6 +3,8 @@ class profile::ghost(
   $ghost_version,
   $ghost_source_dir,
   $ghost_content_dir
+  $ghost_group,
+  $ghost_user,
   $packages,
 ) {
   packages { $packages:
@@ -17,6 +19,8 @@ class profile::ghost(
     ghost_version     => $ghost_version,
     ghost_source_dir  => $ghost_source_dir,
     ghost_content_dir => $ghost_content_dir,
+    ghost_group       => $ghost_group,
+    ghost_user        => $ghost_user,
     require           => [Packages[$packages], Class['::nodejs']]
   }
 }
