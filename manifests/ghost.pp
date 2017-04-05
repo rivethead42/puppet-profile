14462ab30c5e33af452c4ea52010d0d115642c18
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2017-04-05 11:05:43 -0500

    use new ghost module

--- manifests/ghost.pp
+++ manifests/ghost.pp
@@ -1,23 +1,3 @@
-class profile::ghost(
-  $nodejs_version,
-  $ghost_version,
-  $ghost_source_dir,
-  $ghost_content_dir,
-  $ghost_group,
-  $ghost_user,
-  $packages,
-) {
-  package { $packages:
-    ensure => present,
-  }
-  -> class { '::nodejs':
-    repo_url_suffix => $nodejs_version,
-  }
-  -> class { "::ghost":
-    ghost_version     => $ghost_version,
-    ghost_source_dir  => $ghost_source_dir,
-    ghost_content_dir => $ghost_content_dir,
-    ghost_group       => $ghost_group,
-    ghost_user        => $ghost_user,
-  }
+class profile::ghost {
+  ::ghost::blog { 'ghost': }
 }
